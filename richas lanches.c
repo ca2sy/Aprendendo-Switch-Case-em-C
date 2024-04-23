b9313a11831e9cdc042080fd269c40bc45baaae4
Rafaela Sousa Costa <163324726+ca2sy@users.noreply.github.com>
2024-04-23 09:40:31 -0300

    Update richas lanches.c

--- richas lanches.c
+++ richas lanches.c
@@ -3,7 +3,7 @@
 #include <locale.h>
 
 
-//variáveis//
+//variÃ¡veis//
 
 int cod;
 float preco;
@@ -18,7 +18,7 @@ int main (){
 	
 	do {
 printf (" _____________________________________________\n");
-printf ("|LANCHES      |    CÓDIGO      |    PREÇO     |\n");
+printf ("|LANCHES      |    CÃ“DIGO      |    PREÃ‡O     |\n");
 printf ("|_____________________________________________|\n");
 printf ("|Cachorro     |      100       |     4,2      |\n");
 printf ("|quente       |                |              |\n"); 
@@ -45,13 +45,13 @@ printf ("|_____________|________________|______________|\n");
 
 //entrada de dados//
 printf ("                                                 \n");
-printf ("Olá, seja bem vindo ao RICHAS LANCHES!! Por favor informe o código do seu pedido.\n");
+printf ("OlÃ¡, seja bem vindo ao RICHAS LANCHES!! Por favor informe o cÃ³digo do seu pedido.\n");
 scanf ("%d", &cod);
 
 switch (cod)
 {
 	case (100):
-		printf("Informe a quantidade de cachorro-quente que você quer.\n");
+		printf("Informe a quantidade de cachorro-quente que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 4.2);
 		printf ("Seu pedido ficou por $f reais\n", preco);
@@ -59,35 +59,35 @@ switch (cod)
 		break;
 	
 	case (101):
-			printf("Informe a quantidade de bauru simples que você quer.\n");
+			printf("Informe a quantidade de bauru simples que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 3.3);
 		printf ("Seu pedido ficou por %f reais\n", preco);
 			controle1 = 1;
 		break;
     case (102):
-			printf("Informe a quantidade de bauru com ovo que você quer.\n");
+			printf("Informe a quantidade de bauru com ovo que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 5.5);
 		printf ("Seu pedido ficou por %f reais\n", preco);
 			controle1 = 1;
 		break;
 	case (103):
-			printf("Informe a quantidade de hambuguer que você quer.\n");
+			printf("Informe a quantidade de hambuguer que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 6.2);
 		printf ("Seu pedido ficou por %f reais\n", preco);
 			controle1 = 1;
 		break;
     case (104):
-			printf("Informe a quantidade de cheeseburguer que você quer.\n");
+			printf("Informe a quantidade de cheeseburguer que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 5.3);
 		printf ("Seu pedido ficou por %f reais\n", preco);
 		controle1 = 1;
 		break;
 	case (105):
-			printf("Informe a quantidade de refrigerante que você quer.\n");
+			printf("Informe a quantidade de refrigerante que vocÃª quer.\n");
 		scanf ("%d", &quant);
 		preco = (quant * 4.5);
 		printf ("Seu pedido ficou por %f reais\n", preco);
@@ -95,7 +95,7 @@ switch (cod)
 		break;
 	default:
 		controle1 = 0;
-		printf ("Códgo inválido, por favor, tente novamente.\n");
+		printf ("CÃ³dgo invÃ¡lido, por favor, tente novamente.\n");
 		printf ("                                                 \n");
 }
 
@@ -107,6 +107,5 @@ switch (cod)
 
 
 
-//saída de dados//
 
 }
